be1b5691d9dbb2003e6fe4e30a6c1e0a6d6a2981
Sukhendu Mandal <76108642+sukhendu02@users.noreply.github.com>
2025-01-15 11:34:07 +0000

    add dark mode

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -6,41 +6,53 @@ import { NavLink, Link } from "react-router-dom";
 export default function Sidebar() {
   return (
     <>
-      <nav className='sidenav text-sm h-screen fixed shadow-md dark:bg-slate-700'>
+      <nav className='sidenav text-sm h-screen fixed shadow-md dark:bg-slate-800 dark:text-white '>
+        <div className='text-center my-5'>
+          <input className='p-2 rounded-lg border border-solid dark:bg-slate-300 dark:border-slate-600' placeholder="Quick Search" type="text" />
+        </div>
       <NavLink
           to="/"
           style={({isActive})=>({
-            color: isActive ? "#fff" : "",
-            background: isActive ? "#38BDF8" : "",
+            color: isActive ? "#3490dc" : "",
+            // background: isActive ? "#38BDF8" : "",
+            fontWeight: isActive ?"600":"600",
           })}
-          className="block p-3 m-1 hover:bg-slate-100 rounded-2xl">
+          className="block p-2 m-1 hover:bg-slate-100 dark:hover:bg-transparent dark:text-slate-300 rounded-2xl">
           Home
       </NavLink>
       <NavLink
           to="/Project-1"
           style={({isActive})=>({
-            color: isActive ? "#fff" : "",
-            background: isActive ? "#38BDF8" : "",
+
+            color: isActive ? "#3490dc" : "",
+            // background: isActive ? "#38BDF8" : "",
+            fontWeight: isActive ?"600":"600",
+
+            // background: isActive ? "#38BDF8" : "",
           })}
-          className="block p-3 m-1 hover:bg-slate-100 rounded-2xl">
+          className="block p-2 m-1 hover:bg-slate-100 hover:bg-transparent dark:text-slate-300 rounded-2xl">
           Project-1
       </NavLink>
       <NavLink
           to="/Project-2"
           style={({isActive})=>({
-            color: isActive ? "#fff" : "",
-            background: isActive ? "#38BDF8" : "",
+            color: isActive ? "#3490dc" : "",
+            fontWeight: isActive ?"600":"600",
+            // background: isActive ? "#38BDF8" : "",
           })}
-          className="block p-3 m-1 hover:bg-slate-100 rounded-2xl">
+          className="block p-2 m-1 hover:bg-slate-100 hover:bg-transparent dark:text-slate-300 rounded-2xl">
           Project-2
       </NavLink>
       <NavLink
           to="/Project-3"
           style={({isActive})=>({
-            color: isActive ? "#fff" : "",
-            background: isActive ? "#38BDF8" : "",
+            color: isActive ? "#3490dc" : "",
+            // background: isActive ? "#38BDF8" : "",
+            fontWeight: isActive ?"600":"600",
+           
+            // background: isActive ? "#38BDF8" : "",
           })}
-          className="block p-3 m-1 hover:bg-slate-100 rounded-2xl">
+          className="block p-2 m-1 hover:bg-slate-100 hover:bg-transparent dark:text-slate-300 rounded-2xl">
           Project3
       </NavLink>
       </nav>
--- src/Components/Topbar.jsx
+++ src/Components/Topbar.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import IconButton from '@mui/material/IconButton';
 import Sidebar from './Sidebar';
 import MenuIcon from '@mui/icons-material/Menu';
+import LightModeIcon from '@mui/icons-material/LightMode';
 
 export default function Topbar() {
+
+const [mode,setmode] =useState("dark")
+
+
+const changemode=()=>{
+  setmode(mode==="dark"? "light" : "dark")
+}
+
+useEffect(()=>{
+  if(mode==='dark'){
+    document.querySelector('html')?.classList.add('dark');
+  }
+  else{
+    document.querySelector('html')?.classList.remove('dark');
+    
+  }
+  
+},[mode])
+
+
   return (
     <>
-    <nav className='p-1 border-sm shadow-sm'>
+    <nav className='border-sm shadow-sm dark:bg-slate-800 dark:text-white dark:border-slate-200'>
       <div className="flex justify-between items-center py-4">
 
         {/* <Link to="/">Logo</Link> */}
@@ -19,8 +40,10 @@ export default function Topbar() {
         </div>
 
         <div className="">
-            <button className='px-2 mx-2'>Light</button>
-            <button className='px-2 mx-2'>Profile</button>
+            <button onClick={changemode} className='px-2 mx-2 dark:text-slate-50 '>
+              <LightModeIcon className='text-blue-600 dark:text-gray-300'/>
+            </button>
+            <button className='mx-2 px-2 '>Profile</button>
         </div>
       </div>
     </nav>
