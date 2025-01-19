61e6fea30a558dc9298f14805b6936b70a0dd0b0
Sukhendu <sukhendumandal02@gmail.com>
2025-01-19 22:18:08 +0530

    changes in sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -6,7 +6,7 @@ import { NavLink, Link } from "react-router-dom";
 export default function Sidebar() {
   return (
     <>
-      <nav className='sidenav text-sm h-screen fixed shadow-md dark:bg-slate-800 dark:text-white hidden sm:block'>
+      <nav className='sidenav h-screen fixed shadow-md dark:bg-slate-800 dark:text-white hidden sm:block'>
         <div className='text-center my-5'>
           <input className='p-2 rounded-lg border border-solid dark:bg-slate-300 dark:border-slate-600' placeholder="Quick Search" type="text" />
         </div>
@@ -18,8 +18,15 @@ export default function Sidebar() {
             fontWeight: isActive ?"600":"600",
           })}
           className="block p-2 m-1 hover:bg-slate-100 dark:hover:bg-transparent dark:text-slate-300 rounded-2xl">
+
+            <span className='flex items-center'  >
+
             <HomeIcon className='mx-1 p-1 rounded  dark:bg-slate-100 active:bg-black'/>
-          Home
+              <span className='p-2'>
+                Home <br />
+                <small className='text-gray-400'>Dashbord to all the feature we had</small>
+              </span>
+            </span>
       </NavLink>
       <NavLink
           to="/Project-1"
@@ -32,7 +39,16 @@ export default function Sidebar() {
             // background: isActive ? "#38BDF8" : "",
           })}
           className="block p-2 m-1 hover:bg-slate-100 dark:hover:bg-transparent dark:text-slate-300 rounded-2xl">
-          Project-1
+
+          
+          <span className='flex items-center'  >
+
+<HomeIcon className='mx-1 p-1 rounded  dark:bg-slate-100 active:bg-black'/>
+  <span className='p-2'>
+          Text-to-Image<br />
+    <small className='text-gray-400'>Generate image from text</small>
+  </span>
+</span>
       </NavLink>
       <NavLink
           to="/Project-2"
@@ -42,7 +58,15 @@ export default function Sidebar() {
             // background: isActive ? "#38BDF8" : "",
           })}
           className="block p-2 m-1 hover:bg-slate-100 dark:hover:bg-transparent dark:text-slate-300 rounded-2xl">
-          Project-2
+            
+            <span className='flex items-center'  >
+
+            <HomeIcon className='mx-1 p-1 rounded  dark:bg-slate-100 active:bg-black'/>
+              <span className='p-2'>
+          Upscale Image<br />
+                <small className='text-gray-400'>Upscale Image quality </small>
+              </span>
+            </span>
       </NavLink>
       <NavLink
           to="/Project-3"
